77ee226a23f625158bc37e42dce99058a6de252c
Serge Shkurko <shkurko1998@gmail.com>
2018-12-24 23:50:05 +0500

    Add term get by id

---
+++ src/handlers/term/getById.js
@@ -0,0 +1,14 @@
+import response from 'utils/response'
+import getById from 'api/term/getById'
+
+export default async function (event, _context, callback) {
+  try {
+    const result = await getById(decodeURIComponent(event.pathParameters.id))
+    callback(null, response(result))
+  } catch ({ message: error, code = 406 }) {
+    callback(null, response({
+      status: false,
+      error,
+    }, code))
+  }
+}
